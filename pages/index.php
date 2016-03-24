71ff9ab8b7a3c792e1d6ac45e68cc3b786c0657b
GIBB-NIcoleS <nicole.sager@iet-gibb.ch>
2016-03-24 08:57:09 +0100

    Erstellt
    First time Github o.o

---
+++ pages/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+	<meta name="Online Katalog" content="Online Katalog">
+	<link rel="stylesheet" href="../script/styles.css">
+</head>
+<html>
+	<body>
+		<nav>
+			<ul>
+			<li><p class="logo">Online Katalog</p></li>
+			<li><a href="" id="active">HOME</a></li>
+			<li><a href="">Artikel</a></li>
+			<li><a href="">Blog</a></li>
+			<li><a href="">About US</a></li>
+			<li><a href="">LogIn</a></li>
+			</ul>
+		</nav>
+		
+		<div class="inhalt">
+			<h1 class="logo">Silvias Vintage Shop</h1>
+		</br></br></br></br>
+					
+		</div>
+		
+		<footer>
+			<h4>Silvia Sager</h4>
+			<p>Erlach unter den Halden 12</p>
+		</footer>
+	</body>
+</html>
