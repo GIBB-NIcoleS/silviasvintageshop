d72f679ca83016b2d3fae56e53265337d966a01f
GIBB-NIcoleS <nicole.sager@iet-gibb.ch>
2016-03-31 08:49:02 +0200

    31.03.2016
    nav bar switch

---
+++ pages/home.php
@@ -0,0 +1,9 @@
+<h1 class="logo">Silvias Vintage shop</h1>
+			
+			<IMG class="displayed" src="../images/sofa_antik.jpg" alt="Sofa">
+			
+				
+				  
+
+			
+			</br></br></br></br></br></br></br></br>
--- pages/index.php
+++ pages/index.php
@@ -1,27 +1,47 @@
 <!DOCTYPE html>
+
 <head>
 	<meta name="Online Katalog" content="Online Katalog">
 	<link rel="stylesheet" href="../script/styles.css">
+    <link href="../script/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="Javascript" href="./javascript.js">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <html>
 	<body>
-		<nav>
+		<nav >
 			<ul>
-			<li><p class="logo">Online Katalog</p></li>
-			<li><a href="" id="active">HOME</a></li>
-			<li><a href="">Artikel</a></li>
-			<li><a href="">Blog</a></li>
-			<li><a href="">About US</a></li>
-			<li><a href="">LogIn</a></li>
+			<li><p class="logo">Silvias Vintage shop</p></li>
+			<li><a href="?p=home" id="active">HOME</a></li>
+			<li><a href="?p=article">Artikel</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">About US</a></li>
+			<li><a href="#">LogIn</a></li>
 			</ul>
 		</nav>
+<?php 
+	if(isset($_GET['p'] )){
+		$p = $_GET['p'];
+		switch($p){
+		case 'home' :{
+			require_once("home.php");
+		}
 		
-		<div class="inhalt">
-			<h1 class="logo">Silvias Vintage Shop</h1>
-		</br></br></br></br>
-					
-		</div>
+		case 'article' :{
+			require_once("article.php");
+		}
 		
+		default: {
+			require_once("home.php");
+		}
+	}
+	}
+	else{ 
+			require_once("home.php");
+		}
+?>
 		<footer>
 			<h4>Silvia Sager</h4>
 			<p>Erlach unter den Halden 12</p>
