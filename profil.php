1af1cbbac68091a1ddfb9c6f4d48f5a16cdbd4fe
GIBB-NIcoleS <GIBB-NIcoleS@users.noreply.github.com>
2016-05-18 20:54:55 +0200

    OMGGGG

---
+++ profil.php
@@ -0,0 +1,53 @@
+<div class="col-lg-12 form-form">
+	
+	<form class="" name="frmReg"  action="#.php"   method="post"  id="FormR" onsubmit="return">
+  <fieldset>
+    <h2 class="logo">my Profil</h2>
+	
+	
+	<div class="form-group">
+      <label for="inputEmail" class="col-lg-2"><b>Vorname</b></label>
+      <label for="inputEmail" class="col-lg-2 center"><i>Vorname</i></label>
+      <div class="col-lg-11">
+        <input class="form-control edit" id="vorn" name="vorn" placeholder="Vorname" type="text" >
+      </div>
+    </div>
+	<div class="form-group">
+      <label for="inputEmail" class="col-lg-2  "><b>Nachname</b></label>
+      <label for="inputEmail" class="col-lg-2  center"><i>Nachname</i></label>
+      <div class="col-lg-11 ">
+        <input class="form-control edit" id="nachn" name="nachn" placeholder="Nachname" type="text" >
+      </div>
+    </div>
+	<div class="form-group">
+      <label for="inputEmail" class="col-lg-2  "><b>Benutzername</b></label>
+      <label for="inputEmail" class="col-lg-2  center"><i>Benutzername</i></label>
+      <div class="col-lg-11 ">
+        <input class="form-control edit" id="user" name="user" placeholder="Benutzername" type="text" title="Der Benutzername muss genau 6 Zeichen lang sein und darf keine Sonderzeichen beinhalten.">
+      </div>
+    </div>
+	<div class="form-group">
+      <label for="inputEmail" class="col-lg-2  "><b>Email</b></label>
+      <label for="inputEmail" class="col-lg-2  center"><i>Email</i></label>
+      <div class="col-lg-11 ">
+        <input class="form-control edit" id="email" name="email" placeholder="Email" type="text">
+      </div>
+    </div>
+	<div class="form-group">
+      <label for="password" class="col-lg-2  "><b>Passwort</b></label>
+      <label for="password" class="col-lg-2  center"><i>Passwort</i></label>
+      <div class="col-lg-11 ">
+        <input class="form-control edit" id="word" name="word" placeholder="Password" type="password">
+        <input class="form-control edit" id="wordagain" name="wordagain" placeholder="Password" type="password">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-lg-11">
+        <input type="reset" class="btn btn-default" value="Cancel">
+        <input type="submit" class="btn btn-primary" value="Submit">
+      </div>
+    </div>
+  </fieldset>
+  </form>
+  
+ 	
